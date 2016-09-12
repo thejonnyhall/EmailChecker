0b399fb9c914e5d62748e78a455fb39068a9713f
Jonny Hall <jonnyhall@bigpond.com>
2016-09-12 15:07:12 +1000

    Testing out the Kebab-Case
    Also worked out the LinkedIn API sorta

--- startCode.py
+++ startCode.py
@@ -1,1 +1,19 @@
 # Python lols
+# Only-Code-In-Kebab-Case
+
+from linkedin import linkedin
+from oauthlib import *
+
+# Adam's Emails
+# hibblea@qut.edu.au
+# adam.hibble@connect.qut.edu.au
+
+consumer-key = '75kkkkhud4u9fj'
+consumer-secret = '68qABdIFaAXYRYLS'
+return-url = 'http://localhost:8080'
+
+
+authentication = linkedin.LinkedInAuthentication(Consumer-key, consumer-secret, return-url, linkedin.PERMISSIONS.enums.values())
+
+test = application.get_profile()
+print test
